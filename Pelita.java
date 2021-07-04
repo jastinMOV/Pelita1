843fff4d112dc81c40ae1bb5affff1625b62af0b
jastinMOV <77435541+jastinMOV@users.noreply.github.com>
2021-07-04 22:47:15 +0700

    Add files via upload

---
+++ Pelita.java
@@ -0,0 +1,6 @@
+public class Pelita{
+  public static void main(String[] args) {
+    System.out.println("Platform PelItA mempunyai fitur Pengajuan proposal, Pengajuan anggaran, Review & umpan balik, Revisi dan pengesahan, Seminar, Monitoring dan Evaluasi");
+  }
+}
+
